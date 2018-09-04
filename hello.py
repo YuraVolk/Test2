d284e74a2080e78b8ca373aac9b1035ca09bdb88
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2018-09-04 19:18:44 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,25 @@
+def app(environ, start_response):
+
+    # Returns a dictionary in which the values are lists
+
+
+
+    data = environ['QUERY_STRING']
+
+    d = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+
+
+
+    status = '200 OK'
+
+    response_headers = [
+
+        ('Content-type','text/plain'),
+
+        ('Content-Length', str(len(data)))
+
+    ]
+
+    start_response(status, response_headers)
+
+    return d
